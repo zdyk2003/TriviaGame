dd99b0681a57ec828ba98effd229ee3aff8927aa
Zoe Dyksman <zbybee@hotmail.com>
2017-07-19 21:41:01 -0600

    3rd draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,25 @@
-// alert("Goonies Never Die");
-// var startScreen;
-
+//timeout screen not working
+//use the start screen model with the pause before changing screens
+//question won't show up
 $(document).ready(function() {
 
-// 	function firstScreen() {
-// 		startScreen = "<p>Click to Start</p><br><img class = 'image' src = 'images/neverSayDie.jpg>"
-// 		$(".start").html(firstScreen);
-// 	}
-// firstScreen();
-// });
-var correct = 0;
-var inCorrect = 0;
-var unanswered = 0;
+		$(".startImage").on("click", function(e) {
+			audio.play(); 
+			setTimeout(function() {
+			$("#startScreen").hide(); 
+		}, 4000);
+//close startScreen
+		});
+//close document.ready
+});
+
+var startScreen;
+var audio = new Audio("assets/images/Hey you guuuuuuys.mp3");
 
 //create the timer
-
 var timer = 20
-
 var intervalId;
+var unanswered = 0;
 
 function time() {
 	intervalId = setInterval(decrement, 1000);
@@ -29,154 +31,154 @@ function decrement() {
 	$(".timer").html("<p>" + "Time Remaining:  " + timer + "</p>");
 
 	if(timer === 0) {
+		
 		stop();
+				unanswered++; 
+				questionCounter++;
+				console.log(questionCounter); 
+				$(".box").html("<h3>TIME'S UP!</h3><p>The correct answer is Astoria, OR.</p>")
+				$('<img id = "gif" src="assets/images/' + questions.question1.correctImage + '">').appendTo(".box");
+				console.log("no answer");
 	}
 }
 
 function stop() {
-	clearInterval(intervalId);
+	clearInterval(intervalId);	
 }
 
 time();
 
-//array holding the questions
-//array holding the answers
 
-var question1 = {
-	question: "What city does the movie take place?",
-	answer: ["Dallas, TX", "Astoria, OR", "Seattle, WA"],
-	correctAnswer: "Astoria, OR",
+var correct = 0;
+var inCorrect = 0;
+var unanswered = 0;
+var questionCounter = 0;
+
+var questions = {
+	question1: {
+		question: "What city does the movie take place?",
+		answer: ["Dallas, TX", "Astoria, OR", "Seattle, WA"],
+		correctAnswer: "Astoria, OR",
+		correctImage: "astoria.gif"
+	},
+	question2: {
+		question: "What dance does Chunk have to do so he can come in the gate?",
+		answer: ["Truffle Shuffle", "Hokey Pokey", "Macarena"],
+		correctAnswer: "Truffle Shuffle",
+		correctImage: "truffleShuffle.gif"
+	},
+	question3: {
+		question: "The Goonies are searching for the lost treasure of which pirate?",
+		answer: ["Peg Leg Jake", "Captain Jack Sparrow", "One-Eyed Willy"],
+		correctAnswer: "One-Eyed Willy",
+		correctImage: "oneEyedWilly.gif"
+	}
 }
 
-$("#question").text(question1.question);
 
-for (var i = 0; i < question1.answer.length; i++) {
-	$(".box").append("<h3 class = 'answers'>" + question1.answer[i] + "</h3>");
-}
+	// $("#question").html("<h2>" + 'questions' +['question1']+['question'] + "</h2>");
 
-$(".answers").on("click", function(e){
-	var userClick = e.currentTarget.innerText;
+	$(".question1").text(questions.question1.question);
 
-	if (timer === 0) {
-			stop();
-			unanswered++;
-			console.log(unanswered);
-			$(".box").html("<h3>TIME'S UP!</h3><p>The correct answer is Astoria, OR.</p>")
-			$('<img id = "gif" src="assets/images/astoria.gif">').appendTo(".box");
-			console.log("no answer");
-		}	
+	for (var i = 0; i < questions.question1.answer.length; i++) {
+		$(".box").append("<h3 class = 'answers'>" + questions.question1.answer[i] + "</h3>");
+		console.log(questions.question1.answer[i]);
+	}
 
-		else if(userClick === question1.correctAnswer) {
-			stop();
-			correct++;
-			console.log(correct);
-			$(".box").html("<h3>CORRECT!!</h3> <p>Goonies took place in Astoria, OR.</p>");
-			$('<img id = "gif" src="assets/images/astoria.gif">').appendTo(".box");
-			console.log("yay for you")
+	$(".answers").on("click", function(e){
+		var userClick = e.currentTarget.innerText;
 
-		}
-		else if (userClick !== question1.correctAnswer) {
-			stop();
-			inCorrect++;
-			console.log(inCorrect);
-			$(".box").html("<h3>WRONG!</h3><p>The correct answer is Astoria, OR.</p>")
-			$('<img id = "gif" src="assets/images/astoria.gif">').appendTo(".box");
-			console.log("wrong")
-		}
-//close question1		
-});
+			if(userClick === questions.question1.correctAnswer) {
+				stop();
+				correct++;
+				console.log(correct);
+				questionCounter++;
+				console.log(questionCounter);
+				$(".box").html("<h3>CORRECT!!</h3> <p>Goonies took place in Astoria, OR.</p>");
+				$('<img id = "gif" src="assets/images/' + questions.question1.correctImage + '">').appendTo(".box");
+				// setTimeout function(); 
+			}
+			else if (userClick !== questions.question1.correctAnswer) {
+				stop();
+				inCorrect++;
+				console.log(inCorrect);
+				questionCounter++;
+				console.log(questionCounter);
+				$(".box").html("<h3>WRONG!</h3><p>The correct answer is Astoria, OR.</p>")
+				$('<img id = "gif" src="assets/images/' + questions.question1.correctImage + '">').appendTo(".box");
+				console.log("wrong")
+			}
 
-var question2 = {
-	question: "What dance does Chunk have to do so he can come in the gate?",
-	answer: ["Truffle Shuffle", "Hokey Pokey", "Macarena"],
-	correctAnswer: "Truffle Shuffle",
-}
+	});
 
-$("#question").text(question2.question);
+// 	$("#question").text(questions.question2.question);
 
-for (var i = 0; i < question2.answer.length; i++) {
-	$(".box").append("<h3 class = 'answers'>" + question2.answer[i] + "</h3>");
-}
+// 	for (var i = 0; i < questions.question2.answer.length; i++) {
+// 		$(".box").append("<h3 class = 'answers'>" + questions.question2.answer[i] + "</h3>");
 
-$(".answers").on("click", function(e){
-	var userClick = e.currentTarget.innerText;
+// 	}
 
-	if (timer === 0) {
-			stop();
-			unanswered++;
-			console.log(unanswered);
-			$(".box").html("<h3>TIME'S UP!</h3><p>The correct answer is the Truffle Shuffle.</p>")
-			$('<img id = "gif" src="assets/images/truffleShuffle.gif">').appendTo(".box");
-			console.log("no answer");
-		}	
+// 	$(".answers").on("click", function(e){
+// 		var userClick = e.currentTarget.innerText;
 
-		else if(userClick === question2.correctAnswer) {
-			stop();
-			correct++;
-			console.log(correct);
-			$(".box").html("<h3>CORRECT!!</h3> <p>Chunk had to do the Truffle Shuffle.</p>");
-			$('<img id = "gif" src="assets/images/truffleShuffle.gif">').appendTo(".box");
-			console.log("yay for you")
+// 			if(userClick === questions.question2.correctAnswer) {
+// 				stop();
+// 				correct++;
+// 				console.log(correct);
+// 				$(".box").html("<h3>CORRECT!!</h3> <p>Chunk had to do the Truffle Shuffle.</p>");
+// 				$('<img id = "gif" src="assets/images/truffleShuffle.gif">').appendTo(".box");
+// 				console.log("yay for you")
 
-		}
-		else if (userClick !== question2.correctAnswer) {
-			stop();
-			inCorrect++;
-			console.log(inCorrect);
-			$(".box").html("<h3>WRONG!</h3><p>The correct answer is the Truffle Shuffle.</p>")
-			$('<img id = "gif" src="assets/images/truffleShuffle.gif">').appendTo(".box");
-			console.log("wrong")
-		}
-//close question2		
-});
-
-var question3 = {
-	question: "The Goonies are searching for the lost treasure of which pirate?",
-	answer: ["Peg Leg Jake", "Captain Jack Sparrow", "One-Eyed Willy"],
-	correctAnswer: "One-Eyed Willy",
-}
-
-$("#question").text(question3.question);
-
-for (var i = 0; i < question3.answer.length; i++) {
-	$(".box").append("<h3 class = 'answers'>" + question3.answer[i] + "</h3>");
-}
-
-$(".answers").on("click", function(e){
-	var userClick = e.currentTarget.innerText;
-
-	if (timer === 0) {
-			stop();
-			unanswered++;
-			console.log(unanswered);
-			$(".box").html("<h3>TIME'S UP!</h3><p>The correct answer is One-Eyed Willy.</p>")
-			$('<img id = "gif" src="assets/images/oneEyedWilly.gif">').appendTo(".box");
-			console.log("no answer");
-		}	
-
-		else if(userClick === question3.correctAnswer) {
-			stop();
-			correct++;
-			console.log(correct);
-			$(".box").html("<h3>CORRECT!!</h3> <p>The Goonies are searching for One-Eyed Willy's treasure.</p>");
-			$('<img id = "gif" src="assets/images/oneEyedWilly.gif">').appendTo(".box");
-			console.log("yay for you")
-
-		}
-		else if (userClick !== question3.correctAnswer) {
-			stop();
-			inCorrect++;
-			console.log(inCorrect);
-			$(".box").html("<h3>WRONG!</h3><p>The correct answer is One-Eyed Willy.</p>")
-			$('<img id = "gif" src="assets/images/oneEyedWilly.gif">').appendTo(".box");
-			console.log("wrong")
-		}
-//close question3		
-});
+// 			}
+// 			else if (userClick !== questions.question2.correctAnswer) {
+// 				stop();
+// 				inCorrect++;
+// 				console.log(inCorrect);
+// 				$(".box").html("<h3>WRONG!</h3><p>The correct answer is the Truffle Shuffle.</p>")
+// 				$('<img id = "gif" src="assets/images/truffleShuffle.gif">').appendTo(".box");
+// 				console.log("wrong")
+// 			}
+// 	});
 
 
-//close document.ready
-});
+
+// 	$("#question").text(questions.question3.question);
+
+// 	for (var i = 0; i < questions.question3.answer.length; i++) {
+// 		$(".box").append("<h3 class = 'answers'>" + question3.answer[i] + "</h3>");
+// 	}
+
+// 	$(".answers").on("click", function(e){
+// 		var userClick = e.currentTarget.innerText;
+
+// 			if(userClick === questions.question3.correctAnswer) {
+// 				stop();
+// 				correct++;
+// 				console.log(correct);
+// 				$(".box").html("<h3>CORRECT!!</h3> <p>The Goonies are searching for One-Eyed Willy's treasure.</p>");
+// 				$('<img id = "gif" src="assets/images/oneEyedWilly.gif">').appendTo(".box");
+// 				console.log("yay for you")
+
+// 			}
+// 			else if (userClick !== questions.question3.correctAnswer) {
+// 				stop();
+// 				inCorrect++;
+// 				console.log(inCorrect);
+// 				$(".box").html("<h3>WRONG!</h3><p>The correct answer is One-Eyed Willy.</p>")
+// 				$('<img id = "gif" src="assets/images/oneEyedWilly.gif">').appendTo(".box");
+// 				console.log("wrong")
+// 			}
+// 	});
+
+
+// function endGame() {
+// 	if (counter === 3){
+// 		$("correct").html("<h3>" + "You got" + correct + "questons right!" + "</h3>");
+// 		$("inCorrect").html("<h3>" + "You got" + inCorrect + "questons wrong!" + "</h3>");
+// 		$("unanswered").html("<h3>" + "You did not answer" + unanswered + "questons." + "</h3>");
+// 		$('<img id = "gif" src="assets/images/mikeyWell.gif">').appendTo(".box");
+// 	}
+// }
 
 
 
