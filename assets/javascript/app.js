9ccaa838504a32eafdf53deaaddf7b8bf71621e4
Zoe Dyksman <zbybee@hotmail.com>
2017-07-21 09:48:19 -0600

    finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
 
 		$(".startImage").on("click", function(e) {
-			audio.play(); 
+			audio1.play(); 
 			setTimeout(function() {
 			$("#startScreen").hide(); 
 		}, 4000);
@@ -14,8 +14,9 @@ $(document).ready(function() {
 });
 
 var startScreen;
-var audio = new Audio("assets/images/Hey you guuuuuuys.mp3");
-
+var audio1 = new Audio("assets/images/Hey you guuuuuuys.mp3");
+var audio2 = new Audio("assets/images/Lets get outta here.mp3");
+var audio3 = new Audio("assets/images/Laugh.mp3");
 //variables
 var timer = 20
 var intervalId;
@@ -45,8 +46,6 @@ var questions = {
 		correctImage: "oneEyedWilly.gif"
 	}
 }
-	
-
 //timer
 function time() {
 	intervalId = setInterval(decrement, 1000);
@@ -57,29 +56,42 @@ function decrement() {
 
 	$(".timer").html("<p>" + "Time Remaining:  " + timer + "</p>");
 
+	
 	if(timer === 0) {
-		
 		stop();
-				unanswered++; 
-				$(".box").html("<h3>TIME'S UP!</h3><p>The correct answer is " + questions['question' + questionCounter].correctAnswer + "</p>");
-				$('<img id = "gif" src="assets/images/' + questions['question' + questionCounter].correctImage + '">').appendTo(".box");
-				console.log("no answer");
+		unanswered++;
+		console.log(unanswered);
+		$(".box").html("<h3>TIME'S UP!</h3><p>The correct answer is " + questions['question' + questionCounter].correctAnswer + "</p>");
+		$('<img id = "gif" src="assets/images/' + questions['question' + questionCounter].correctImage + '">').appendTo(".box");
+		console.log("no answer");
+
+			if(unanswered === 3) { //no answer clicks
+
+				setTimeout(function() {
+					stop();
+					$(".box").html("");
+					endGame2();
+					}, 2500);
+			}
+			else{
 				setTimeout(function() {
 					$(".box").html("");
 					$("#gif").attr("src", "");
-				questionCounter++;
-				console.log(questionCounter); 
-				time();
-				timer = 20;		
-				$("#question").text(questions['question' + questionCounter].question);
-					for (var i = 0; i < questions['question' + questionCounter].answer.length; i++) {
-						$(".box").append("<h3 class = 'answers'>" + questions['question' + questionCounter].answer[i] + "</h3>");
-						console.log(questions['question' + questionCounter].answer[i]);
-					}
-
-				}, 3000);
-	}
-}
+					questionCounter++;
+					console.log(questionCounter); 
+					time();
+					timer = 20;		
+					$("#question").text(questions['question' + questionCounter].question);
+						for (var i = 0; i < questions['question' + questionCounter].answer.length; i++) {
+							$(".box").append("<h3 class = 'answers'>" + questions['question' + questionCounter].answer[i] + "</h3>");
+							console.log(questions['question' + questionCounter].answer[i]);
+						}
+					}, 3000);
+			}
+		
+	} //close if (timer === 0)
+	
+}  //close function decrement
 
 function stop() {
 	clearInterval(intervalId);	
@@ -115,8 +127,6 @@ time();
 				console.log(inCorrect);
 				$(".box").html("<h3>WRONG!</h3><p>The correct answer is  " + questions['question' + questionCounter].correctAnswer + ".</p>")
 				$('<img id = "gif" src="assets/images/' + questions['question' + questionCounter].correctImage + '">').appendTo(".box");
-				// setTimeout(checkIfDone, 4000);
-
 				questionCounter++;
 				console.log(questionCounter);
 			}
@@ -126,23 +136,25 @@ time();
 	});
 
 function checkIfDone (){
-	// var counter = 1;
-	if(questionCounter === 4){
-		//there are 3 questions
-setTimeout(function() {
-		$("#question").text(questions['question' + questionCounter].question);
-			for (var i = 0; i < questions['question' + questionCounter].answer.length; i++) {
-				$(".box").append("<h3 class = 'answers'>" + questions['question' + questionCounter].answer[i] + "</h3>");
-				console.log(questions['question' + questionCounter].answer[i]);
-			}
-		}, 3000);
-		endGame();
-	}
 
-	else{
+	if(questionCounter === 4){
+			
+			setTimeout(function() {
+				stop();
+				endGame1();
+			}, 2500);
+
+	}
+	// else if (unanswered === 3) {
+
+	// 	setTimeout(function() {
+	// 		stop();
+	// 		endGame2();
+	// 		}, 2500);
+	// }
+	else {
+
 		time();
-		timer = 20;
-	
 		setTimeout(function() {
 		$(".box").html("");
 		$("#gif").attr("src", "");
@@ -153,24 +165,48 @@ setTimeout(function() {
 				console.log(questions['question' + questionCounter].answer[i]);
 			}
 		}, 3000);
-		console.log("counter is " + questionCounter);
+
+		timer = 20;
+		
 	}
 	
 }
 
-function endGame() {
-		console.log("it's over");
+function endGame1() {
+		// console.log("it's over");
 		$("#startScreen").html("");
+		$(".instructions").html("");
 		$(".timer").empty();
+		$("#question").text("");
 		$(".box").html("");
 		$("#gif").attr("src", "");
-		$("correct").html("<h3>" + "You got" + correct + "questons right!" + "</h3>");
-		$("inCorrect").html("<h3>" + "You got" + inCorrect + "questons wrong!" + "</h3>");
-		$("unanswered").html("<h3>" + "You did not answer" + unanswered + "questons." + "</h3>");
+		audio3.play();
+		$("#question").html("<h3>" + "You answered " + correct + " right" + "</h3>"  +
+		"<h3>" + "You answered " + inCorrect + " wrong" + "</h3>" + 
+		"<h3>" + "You had " + unanswered + " unanswered" + "</h3>");
 		$('<img id = "gif" src="assets/images/mikeyWell.gif">').appendTo(".box");
+		setTimeout(function() {
+			location.reload();
+		}, 5000);
+}
 
+function endGame2() {
+		console.log("it's over");
+		$("#startScreen").html("");
+		$(".instructions").html("");
+		$(".timer").empty();
+		$("#question").text("");
+		$(".box").html("");
+		$("#gif").attr("src", "");
+		audio2.play();
+		$("#question").html("<h3>" +  "You got didn't answer any questions!" + "<br>" + "You can't be a Goonie." + "</h3>");
+		$('<img id = "gif" src="assets/images/mikeyWell.gif">').appendTo(".box");
+		setTimeout(function() {
+			location.reload();
+		}, 5000);	
 
 }
+
 
 
 
